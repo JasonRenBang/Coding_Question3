3adc329bae06b206e0112debbc9578b5aa2fa1ea
Zhichen Ren <zhichenr@student.unimelb.edu.au>
2023-09-24 15:19:52 +1000

    question 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch, Provider } from 'react-redux'
+import { TreeSelect } from 'antd'
+import { configureStore } from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga'
+import { takeLatest, call, put } from 'redux-saga/effects'
+import axios from 'axios'
+import { createSlice } from '@reduxjs/toolkit'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState: [],
+  reducers: {
+    fetchRequest: state => state,
+    fetchSuccess: (state, action) => action.payload,
+    fetchFailure: state => state,
+  },
+})
+
+const { fetchRequest, fetchSuccess, fetchFailure } = categoriesSlice.actions
+
+
+function* fetchCategoriesSaga () {
+  try {
+    //change the API_ENDPOINT to the right url
+    const response = yield call(axios.get, 'API_ENDPOINT')
+    yield put(fetchSuccess(response.data))
+  } catch (error) {
+    yield put(fetchFailure(error))
+  }
 }
 
-export default App;
+function* watchFetchCategories () {
+  yield takeLatest(fetchRequest.type, fetchCategoriesSaga)
+}
+
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store = configureStore({
+  reducer: {
+    categories: categoriesSlice.reducer,
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(sagaMiddleware),
+})
+
+sagaMiddleware.run(watchFetchCategories)
+
+const App = () => {
+  const dispatch = useDispatch()
+  const categories = useSelector(state => state.categories)
+  const [treeData, setTreeData] = useState([])
+
+  useEffect(() => {
+    dispatch(fetchRequest())
+  }, [dispatch])
+
+  useEffect(() => {
+    if (categories.length > 0) {
+      const convertedData = convertToTreeData(categories)
+      setTreeData(convertedData)
+    }
+  }, [categories])
+
+  const handleSelect = value => {
+    alert(`Selected category ID: ${value}`)
+  }
+
+
+  const convertToTreeData = (categories) => {
+    return categories.map(category => {
+      const node = {
+        title: category.name,
+        value: category.categoryId,
+        key: category.categoryId,
+      }
+      if (category.children && category.children.length > 0) {
+        node.children = convertToTreeData(category.children)
+      }
+      return node
+    })
+  }
+
+  return (
+    <div className="App">
+      <TreeSelect
+        showSearch
+        style={{ width: '100%' }}
+        dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+        placeholder="Please select a category"
+        allowClear
+        treeDefaultExpandAll
+        onChange={handleSelect}
+        treeData={treeData}
+      />
+    </div>
+  )
+}
+
+export default () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
